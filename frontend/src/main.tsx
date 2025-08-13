fd0a144fa096a0e1c8ec524231c7d63f4bc92a3d
Abhudaya Singh <95404646+singhabhudaya@users.noreply.github.com>
2025-08-13 10:26:10 +0530

    Update main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,15 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+} else {
+  console.error("Root element not found. Make sure your index.html has a <div id='root'></div>")
+}
